ba4c2aa0db0116a1dfef43a19942ed2267b97d77
alfie-ns <alfienurse@gmail.com>
2024-05-26 00:23:45 +0100

    update

--- initvenv.sh
+++ initvenv.sh
@@ -5,4 +5,8 @@ source venv/bin/activate #activate venv
 pip3 install -r requirements.txt #install required dependencies
 
 echo "Setup complete"
-ech
+echo "Starting Django Server on port 8000"
+
+python3 manage.py runserver 0.0.0.0:8000 
+
+
