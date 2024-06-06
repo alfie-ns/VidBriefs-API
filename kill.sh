738ad7246ba786d1612b1bd8fc89ef39a84fa0d0
alfie-ns <alfienurse@gmail.com>
2024-06-06 01:07:00 +0100

    update

--- kill.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash # shebang
-./push.sh # push changes 1st
-cd .. # go back to API directory
-rm -rf VidBriefs-API # remove the API directory
---
+++ pbd.sh
@@ -0,0 +1,19 @@
+# 1. Push
+# 2. Back out of dir
+# 3. Delete repo
+#!/bin/bash
+
+# Get the current directory name
+current_dir=$(basename "$PWD")
+
+# Run the push script
+if ./push.sh; then
+  # Change to the parent directory if push.sh succeeds
+  cd ..
+  # Remove the original directory
+  rm -rf "$current_dir"
+  echo "Process completed successfully."
+else
+  echo "Error: push.sh failed. Exiting."
+fi
+
