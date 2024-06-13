f9708c4f537540691caff7d33922aab320357da1
alfie-ns <alfienurse@gmail.com>
2024-06-13 23:57:13 +0100

    update

--- start.sh
+++ start.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
+# ------------------------------------ACTIVATE VIRTUAL ENVIRONMENT-------------------------------------
 # Check if the virtual environment directory exists
 if [ -d "venv" ]; then
+    # Remove existing virtual environment
+    rm -rf venv
+    echo "Creating virtual environment 'venv'..."
+    # Create the virtual environment
+    python3 -m venv venv
     # Activate the virtual environment
     source venv/bin/activate
 else
     echo "Creating virtual environment 'venv'..."
+    # Create the virtual environment
     python3 -m venv venv
+    # Activate the virtual environment
     source venv/bin/activate
-fi
+fi # End the if-then statement
+# -----------------------------------------------------------------------------------------------------
 
-# Upgrade pip
+# ------------------------------------INSTALL REQUIRED PACKAGES----------------------------------------
+# Upgrade pip to the latest version
 pip install --upgrade pip
 
-# Install required packages
+# Install required packages from requirements.txt
 pip install -r requirements.txt
+# The venv holds the required packages for the project
+# -----------------------------------------------------------------------------------------------------
 
+# ------------------------------------SETUP DJANGO PROJECT---------------------------------------------
+# Migrate the database
 echo -e "\nMigrating Database"
 python3 manage.py migrate
 
+# Create a superuser for Django admin
 echo -e "\nCreating Superuser"
 python3 manage.py createsuperuser
+# -----------------------------------------------------------------------------------------------------
 
+# ------------------------------------START SERVER-----------------------------------------------------
+# Setup is complete
 echo -e "\nSetup complete"
-echo -e "\nStarting Django Server on port 8000"
+# Start the Django server accessible to all IP addresses on port 8000
+echo -e "\nStarting Django Server, accessible to all IP addresses on port 8000"
 python3 manage.py runserver 0.0.0.0:8000
 
-# Deactivate the virtual environment
+# -----------------------------------------------------------------------------------------------------
+
+# Deactivate the virtual environment to clean up and avoid conflicts
 deactivate
