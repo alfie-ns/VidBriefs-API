5796049e6880c5e8c8a3ab02d696f892612696c2
alfie-ns <alfienurse@gmail.com>
2024-06-19 00:37:47 +0100

    update

--- response/views.py
+++ response/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .get_youtube_transcript import get_youtube_transcript
+from response import get_youtube_transcript
 from .youtube_api import get_youtube_data
 from django.http import JsonResponse
 import os
