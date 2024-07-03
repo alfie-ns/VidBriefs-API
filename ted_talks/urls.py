d79327cf5d62422a5ad80424e34839c0e5d65cff
Alfie Nurse <100518222+alfie-ns@users.noreply.github.com>
2024-07-03 17:30:45 +0100

    Update urls.py

--- ted_talks/urls.py
+++ ted_talks/urls.py
@@ -5,7 +5,7 @@ from .views import get_tedtalk_transcripts, list_all_talks, recommend_talks
 from . import views
 
 urlpatterns = [
-    path('get_tedtalk_transcripts/', get_tedtalk_transcripts, name='get_tedtalk_transcript'),
+    path('get_tedtalk_transcript/', get_tedtalk_transcripts, name='get_tedtalk_transcript'),
     path('list_all_talks/', list_all_talks, name='list_all_talks'),
     path('recommend_talks/', recommend_talks, name='recommend_talks'),
     path('recommendations/', views.get_recommendations, name='get_recommendations'),
